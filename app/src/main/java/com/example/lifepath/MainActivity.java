94332896ec25050a1dce5171c97e497ba763c014
Danval-003 <77825594+Danval-003@users.noreply.github.com>
2021-09-28 23:17:43 -0600

    VersionMejorada

--- app/src/main/java/com/example/lifepath/MainActivity.java
+++ app/src/main/java/com/example/lifepath/MainActivity.java
@@ -11,6 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(MainActivity.this, Login.class);
+                startActivity(intent);
+            }
+        },2000);
     }
 }
