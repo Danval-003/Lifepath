483388d511336037865808ab5eb533a373c42e88
Danval-003 <77825594+Danval-003@users.noreply.github.com>
2021-09-28 22:13:06 -0600

    VersionSplashScreen

--- app/src/main/java/com/example/lifepath/MainActivity.java
+++ app/src/main/java/com/example/lifepath/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.lifepath;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
